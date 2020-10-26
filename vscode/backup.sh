c6c541d79a53f3b1884a030e8d07d300458e1d64
Hans Bala <hansbala@Hans-MBP.local>
2020-10-26 14:50:45 -0400

    new changes to vs code config

--- vscode/backup.sh
+++ vscode/backup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-SETTINGS_LOCATION=$HOME/"Library/Application Support/Code/User/"
+SETTINGS_LOCATION=$HOME/Library/Application\ Support/Code/User
 
 # Update the installed extensions
 code --list-extensions > extensions.txt
