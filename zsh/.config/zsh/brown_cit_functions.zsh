64dc1fd08324d4f479eab09145d8e90a4b08b83f
Hans Bala <hansbala@Hans-MBP.local>
2020-10-31 19:30:19 -0400

    modified cit mount function to use pk-ssh (key value pairs)

--- zsh/.config/zsh/brown_cit_functions.zsh
+++ zsh/.config/zsh/brown_cit_functions.zsh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+# Deprecated:= using key value pairs now with pk-ssh
 # Function to ssh into hbala's CIT account on CIT machines
 # cit_hans () {
 #   PSS_PATH=~/.config/passwords/.brown_password.txt
@@ -9,20 +10,17 @@
 
 # Mounts the Brown CS department file system to ~/Documents/local_mnt/CIT/
 # A prerequisite for this is for OS X Fuse to be installed on the computer
-cit_mount () {
+citmount () {
   MNT_PATH=~/Documents/local_mnt/CIT
-  PSS_PATH=~/.config/passwords/.brown_password.txt
-  USER_NAM=hbala
-  SRV_PATH=hbala@ssh.cs.brown.edu
-  VOL_NAME=CIT
+  SRV_PATH=hbala@pk-ssh.cs.brown.edu
   RES=`mount | grep -w "CIT" | wc -l`
+
   if [ $RES -gt 0 ]; then
     echo "CIT already mounted to local filesystem"
   else
     mkdir -p $MNT_PATH
-    cat $PSS_PATH | sshfs -o reconnect $SRV_PATH:/home/$USER_NAM/ $MNT_PATH \
-    -ovolname=$VOL_NAME -o password_stdin \
-    && \
+    sshfs -o reconnect $SRV_PATH:/home/hbala $MNT_PATH -ovolname=CIT \
+      && \
     echo "Successfully mounted CIT to local filesystem"
   fi
 }
