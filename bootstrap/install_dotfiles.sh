24d93d15ccbc0cbbe066b381dec5a349de4001c4
Hans <hansbala@hansbala.com>
2026-05-07 13:35:48 -0700

    sync

--- bootstrap/install_dotfiles.sh
+++ bootstrap/install_dotfiles.sh
@@ -6,4 +6,4 @@ ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
 
 cd "$ROOT_DIR"
 
-stow --restow --target="$HOME" ghostty git nix npm nvim starship tmux wall zsh bin agents
+stow --restow --target="$HOME" ghostty git nix npm nvim starship tmux wall zsh bin agents lazygit
